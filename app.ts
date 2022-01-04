0e398f52789f9e2665ed65e81b23fa10196ed2cd
KevinIsYi <rodriguezkevin95g@gmail.com>
2022-01-03 20:10:56 -0600

    Initial bot configuration

---
+++ app.ts
@@ -0,0 +1,16 @@
+import TelegramBot from 'node-telegram-bot-api';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const token = process.env.TELEGRAM_TOKEN;
+
+const bot = new TelegramBot(token!, { polling: true });
+
+bot.on("message", (msg) => {
+    const chatId = msg.chat.id;
+
+    bot.sendMessage(chatId, "Hey! I don't know how you are or what you want. But I like you, just for being here with me.")
+
+});
+
