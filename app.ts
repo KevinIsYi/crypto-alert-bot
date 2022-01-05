98a0cc7e730668c00871bc1d018bf74a358d2b27
KevinIsYi <rodriguezkevin95g@gmail.com>
2022-01-04 22:02:36 -0600

    Uncommented telegram bot section

--- app.ts
+++ app.ts
@@ -1,24 +1,20 @@
-// process.env.NTBA_FIX_319 = "1";
+process.env.NTBA_FIX_319 = "1";
 
 import { CryptoPrices } from "./models/CryptoPrices";
 import { CryptoSymbols } from "./models/CryptoSymbols";
 
-// import TelegramBot from 'node-telegram-bot-api';
-// import dotenv from 'dotenv';
-// import { CryptoSymbols } from './models/CryptoSymbols';
+import TelegramBot from 'node-telegram-bot-api';
+import dotenv from 'dotenv';
 
-// dotenv.config();
+dotenv.config();
 
-// const token = process.env.TELEGRAM_TOKEN;
+const token = process.env.TELEGRAM_TOKEN;
+const bot = new TelegramBot(token!, { polling: true });
 
-// const bot = new TelegramBot(token!, { polling: true });
-
-// bot.on("message", (msg) => {
-//     const chatId = msg.chat.id;
-
-//     bot.sendMessage(chatId, "Hey! I don't know how you are or what you want. But I like you, just for being here with me.")
-// });
-
+bot.on("message", (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, "Hey! I don't know how you are or what you want. But I like you, just for being here with me.")
+});
 
 const main = async() => {
     const coingeckoCryptos = new CryptoSymbols();
@@ -29,4 +25,4 @@ const main = async() => {
     cryptoBot.start();
 }
 
-main();
+// main();
